705a16bbaeb189cac217bd1961c953f6b7172280
kkdeok <kkdeok26@gmail.com>
2024-05-13 17:25:28 +0900

    Add some for istio

--- main.tf
+++ main.tf
@@ -83,6 +83,25 @@ module "eks" {
       desired_size = 15
     }
   }
+
+  node_security_group_additional_rules = {
+    ingress_15017 = {
+      description                   = "${local.cluster_name} Cluster API - Istio Webhook namespace.sidecar-injector.istio.io"
+      protocol                      = "TCP"
+      from_port                     = 15017
+      to_port                       = 15017
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+    ingress_15012 = {
+      description                   = "${local.cluster_name} Cluster API to nodes ports/protocols"
+      protocol                      = "TCP"
+      from_port                     = 15012
+      to_port                       = 15012
+      type                          = "ingress"
+      source_cluster_security_group = true
+    }
+  }
 }
 
 
